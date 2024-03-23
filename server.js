27936b605778ed2401db8d9e44734a54cf724536
Weston Clark <westonclark393@gmail.com>
2024-03-23 11:09:46 -0400

    It was my fault not cors

--- server.js
+++ server.js
@@ -64,18 +64,13 @@ app.get('/stats/all', function(req, res){
     });
 });
 app.get('/stats/add/:playerId', async(req, res) => {
-    try {
-        playerId = '#'+req.params.playerId;
-        console.log(req.params.playerId);
-        await getPlayerStats().then(stats => {
-            let brawlAccount = new BrawlAccount(stats.name, stats.tag, stats.icon, stats.trophies, stats.highestTrophies, stats.expLevel, stats.totalVictories, stats.trioVictories, stats.soloVictories, stats.duoVictories, stats.bestRoboRumbleTime, stats.bestTimeAsBigBrawler, stats.club, stats.nameColor);
-            addToDB(brawlAccount);
-            res.send(JSON.stringify(brawlAccount));
-        });
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
+    playerId = '#'+req.params.playerId;
+    console.log(req.params.playerId);
+    await getPlayerStats().then(stats => {
+        let brawlAccount = new BrawlAccount(stats.name, stats.tag, stats.icon, stats.trophies, stats.highestTrophies, stats.expLevel, stats.totalVictories, stats.trioVictories, stats.soloVictories, stats.duoVictories, stats.bestRoboRumbleTime, stats.bestTimeAsBigBrawler, stats.club, stats.nameColor);
+        addToDB(brawlAccount);
+        res.send(JSON.stringify(brawlAccount));
+     });
 });
 function addToDB(brawlAccount){
     var sqlQuery = "INSERT INTO BrawlAccounts (name, tag, icon, trophies, highestTrophies, expLevel, totalVictories, victories, soloVictories, duoVictories, bestRoboRumbleTime, bestTimeAsBigBrawler, club, color) VALUES ('"+brawlAccount.name+"', '"+brawlAccount.tag+"', '"+brawlAccount.icon+"', "+brawlAccount.trophies+", "+brawlAccount.highestTrophies+", "+brawlAccount.expLevel+", "+brawlAccount.totalVictories+", "+brawlAccount.victories+", "+brawlAccount.soloVictories+", "+brawlAccount.duoVictories+", "+brawlAccount.bestRoboRumbleTime+", "+brawlAccount.bestTimeAsBigBrawler+", '"+brawlAccount.club.name+"', '"+brawlAccount.color+"')";
